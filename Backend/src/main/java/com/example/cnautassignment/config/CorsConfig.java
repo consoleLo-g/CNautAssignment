e8645fef575d4270173a4f9d463410c9bfc11f91
gaurav@xjhzn.onmicrosoft.com <gaurav@xjhzn.onmicrosoft.com>
2025-10-20 18:40:23 +0530

    Updated CORS config

--- Backend/src/main/java/com/example/cnautassignment/config/CorsConfig.java
+++ Backend/src/main/java/com/example/cnautassignment/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://cnautassignment-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
